f748b3ec66985407ea6c74e93339aa2a5f4e3762
Michael Dahl <micdah@gmail.com>
2017-09-12 22:52:13 +0200

    Refactored API Manager
    Re-arranged code a bit, and changed return type to an enumeration and using Linq to defer execution

--- RtMidi.Core/Unmanaged/RtMidiApiManager.cs
+++ RtMidi.Core/Unmanaged/RtMidiApiManager.cs
@@ -2,6 +2,8 @@
 using System.Runtime.InteropServices;
 using RtMidi.Core.Unmanaged.API;
 using Serilog;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace RtMidi.Core.Unmanaged
 {
@@ -13,9 +15,9 @@ namespace RtMidi.Core.Unmanaged
         /// Get array of available RtMidi API's (if any)
         /// </summary>
         /// <returns>The available apis.</returns>
-        public static RtMidiApi[] GetAvailableApis()
+        public static IEnumerable<RtMidiApi> GetAvailableApis()
         {
-            var ptr = IntPtr.Zero;
+            var apisPtr = IntPtr.Zero;
 
             try
             {
@@ -25,41 +27,31 @@ namespace RtMidi.Core.Unmanaged
 
                 // Get array of available API's
                 var enumSize = RtMidiC.Utility.SizeofRtMidiApi();
-                ptr = Marshal.AllocHGlobal(count * enumSize);
-                RtMidiC.GetCompiledApi(ref ptr);
+                apisPtr = Marshal.AllocHGlobal(count * enumSize);
+                RtMidiC.GetCompiledApi(ref apisPtr);
 
-                // Convert to managed enum typess
-                var ret = new RtMidiApi[count];
+                // Convert to managed enum types
                 switch (enumSize)
                 {
                     case 1:
                         var bytes = new byte[count];
-                        Marshal.Copy(ptr, bytes, 0, bytes.Length);
-                        for (var i = 0; i < bytes.Length; i++)
-                            ret[i] = (RtMidiApi)bytes[i];
-                        break;
+                        Marshal.Copy(apisPtr, bytes, 0, bytes.Length);
+                        return bytes.Select(b => (RtMidiApi)b);
                     case 2:
                         var shorts = new short[count];
-                        Marshal.Copy(ptr, shorts, 0, shorts.Length);
-                        for (var i = 0; i < shorts.Length; i++)
-                            ret[i] = (RtMidiApi)shorts[i];
-                        break;
+                        Marshal.Copy(apisPtr, shorts, 0, shorts.Length);
+                        return shorts.Select(s => (RtMidiApi)s);
                     case 4:
                         var ints = new int[count];
-                        Marshal.Copy(ptr, ints, 0, ints.Length);
-                        for (var i = 0; i < ints.Length; i++)
-                            ret[i] = (RtMidiApi)ints[i];
-                        break;
+                        Marshal.Copy(apisPtr, ints, 0, ints.Length);
+                        return ints.Select(i => (RtMidiApi)i);
                     case 8:
                         var longs = new long[count];
-                        Marshal.Copy(ptr, longs, 0, longs.Length);
-                        for (var i = 0; i < longs.Length; i++)
-                            ret[i] = (RtMidiApi)longs[i];
-                        break;
+                        Marshal.Copy(apisPtr, longs, 0, longs.Length);
+                        return longs.Select(l => (RtMidiApi)l);
                     default:
-                        throw new NotSupportedException("sizeof RtMidiApi is unexpected: " + enumSize);
+                        throw new NotSupportedException($"Unexpected size of RtMidiApi enum {enumSize}");
                 }
-                return ret;
             }
             catch (Exception e)
             {
@@ -69,9 +61,9 @@ namespace RtMidi.Core.Unmanaged
             }
             finally
             {
-                if (ptr != IntPtr.Zero)
+                if (apisPtr != IntPtr.Zero)
                 {
-                    Marshal.FreeHGlobal(ptr);
+                    Marshal.FreeHGlobal(apisPtr);
                 }
             }
         }
