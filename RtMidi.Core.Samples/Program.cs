5088aa2c1676b14a3712c766bcd2bcdb31817972
Michael Dahl <micdah@gmail.com>
2017-09-16 13:48:55 +0200

    Renamed Test project to Samples project

---
+++ RtMidi.Core.Samples/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using RtMidi.Core.Unmanaged;
+using RtMidi.Core.Unmanaged.Devices;
+
+namespace RtMidi.Core.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+        }
+
+        private readonly IRtMidiInputDevice _inputDevice;
+
+        public Program() 
+        {
+            Console.WriteLine("Available MIDI API's:");
+            var apis = RtMidiApiManager.GetAvailableApis();
+            foreach (var api in apis)
+                Console.WriteLine($"API: {api}");
+
+            Console.WriteLine("Available MIDI devices:");
+            var inputDevices = RtMidiDeviceManager.Instance.InputDevices.ToList();
+            foreach (var device in inputDevices)
+                Console.WriteLine($"Input Device: {device.Name}:{device.Port}");
+
+            var outputDevices = RtMidiDeviceManager.Instance.OutputDevices.ToList();
+            foreach (var device in outputDevices)
+                Console.WriteLine($"Output Device: {device.Name}:{device.Port}");
+
+            _inputDevice = inputDevices.First().CreateDevice();
+
+            try {
+                _inputDevice.Message += InputDevice_Message;
+                _inputDevice.Open();
+
+                Console.ReadLine();
+            }
+            finally
+            {
+                _inputDevice.Dispose();
+            }
+        }
+
+        private void InputDevice_Message(object sender, byte[] message)
+        {
+            var msg = string.Join(" ", message.Select(b => $"{b:X2}/{b}"));
+            Console.WriteLine($"Received: {msg} (length {message.Length})");
+        }
+    }
+}
