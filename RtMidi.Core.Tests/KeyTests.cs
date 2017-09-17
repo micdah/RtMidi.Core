bd28a19aab09997cce4843409cd30bbe8541ee58
Michael Dahl <micdah@gmail.com>
2017-09-17 20:23:56 +0200

    Renamed Pitch to Key

---
+++ RtMidi.Core.Tests/KeyTests.cs
@@ -0,0 +1,103 @@
+﻿using Xunit;
+using RtMidi.Core.Enums;
+using System;
+
+namespace RtMidi.Core.Tests
+{
+    public class KeyTests
+    {
+        public class OctaveTests
+        {
+            [Fact]
+            public void Should_Throw_ArgumentOutOfRange_When_Key_Below_0()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var key = (Key)(-1);
+                    key.Octave();
+                });
+            }
+
+            [Fact]
+            public void Should_Throw_ArgumentOutOfRange_When_Key_Above_127()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var key = (Key)(128);
+                    key.Octave();
+                });
+            }
+
+            [Fact] public void Should_Return_0_For_Key_0_Through_11() => TestOctave(0, 11, 0);
+            [Fact] public void Should_Return_1_For_Key_12_Through_23() => TestOctave(12, 23, 1);
+            [Fact] public void Should_Return_2_For_Key_24_Through_35() => TestOctave(24, 35, 2);
+            [Fact] public void Should_Return_3_For_Key_36_Through_47() => TestOctave(36, 47, 3);
+            [Fact] public void Should_Return_4_For_Key_48_Through_59() => TestOctave(48, 59, 4);
+            [Fact] public void Should_Return_5_For_Key_60_Through_71() => TestOctave(60, 71, 5);
+            [Fact] public void Should_Return_6_For_Key_72_Through_83() => TestOctave(72, 83, 6);
+            [Fact] public void Should_Return_7_For_Key_84_Through_95() => TestOctave(84, 95, 7);
+            [Fact] public void Should_Return_8_For_Key_96_Through_107() => TestOctave(96, 107, 8);
+            [Fact] public void Should_Return_9_For_Key_108_Through_119() => TestOctave(108, 119, 9);
+            [Fact] public void Should_Return_10_For_Key_120_Through_127() => TestOctave(120, 127, 10);
+
+            private static void TestOctave(int keyFrom, int keyTo, int expectedOctate)
+            {
+                for (var i = keyFrom; i <= keyTo; i++)
+                {
+                    var key = (Key)i;
+                    Assert.Equal(expectedOctate, key.Octave());
+                }
+            }
+        }
+
+        public class NoteTests
+        {
+            [Fact]
+            public void Should_Throw_ArgumentOutOfRange_When_Key_Below_0() 
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var key = (Key)(-1);
+                    key.Note();
+                });
+            }
+
+            [Fact]
+            public void Should_Throw_ArgumentOutOfRange_When_Key_Above_127()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var key = (Key)(128);
+                    key.Note();
+                });
+            }
+
+            [Fact] public void Should_Return_Notes_In_Octave_0() => TestNotes(0);
+            [Fact] public void Should_Return_Notes_In_Octave_1() => TestNotes(1);
+            [Fact] public void Should_Return_Notes_In_Octave_2() => TestNotes(2);
+            [Fact] public void Should_Return_Notes_In_Octave_3() => TestNotes(3);
+            [Fact] public void Should_Return_Notes_In_Octave_4() => TestNotes(4);
+            [Fact] public void Should_Return_Notes_In_Octave_5() => TestNotes(5);
+            [Fact] public void Should_Return_Notes_In_Octave_6() => TestNotes(6);
+            [Fact] public void Should_Return_Notes_In_Octave_7() => TestNotes(7);
+            [Fact] public void Should_Return_Notes_In_Octave_8() => TestNotes(8);
+            [Fact] public void Should_Return_Notes_In_Octave_9() => TestNotes(9);
+            [Fact] public void Should_Return_Notes_In_Octave_10() => TestNotes(10);
+
+            private static void TestNotes(int octave) 
+            {
+                var keyFrom = octave * 12;
+
+                for (var noteNum = 0; noteNum <= 11; noteNum++)
+                {
+                    var keyNum = keyFrom + noteNum;
+                    if (keyNum > 127) return;
+
+                    var expectedNote = (Note)noteNum;
+                    var key = (Key)(keyNum);
+                    Assert.Equal(expectedNote, key.Note());
+                }
+            }
+        }
+    }
+}
---
+++ RtMidi.Core/Enums/Key.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace RtMidi.Core.Enums
+{
+    public enum Key
+    {
+        Key_0, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6, Key_7, Key_8, Key_9, Key_10, Key_11,
+        Key_12, Key_13, Key_14, Key_15, Key_16, Key_17, Key_18, Key_19, Key_20, Key_21, Key_22, Key_23, 
+        Key_24, Key_25, Key_26, Key_27, Key_28, Key_29, Key_30, Key_31, Key_32, Key_33, Key_34, Key_35,
+        Key_36, Key_37, Key_38, Key_39, Key_40, Key_41, Key_42, Key_43, Key_44, Key_45, Key_46, Key_47,
+        Key_48, Key_49, Key_50, Key_51, Key_52, Key_53, Key_54, Key_55, Key_56, Key_57, Key_58, Key_59,
+        Key_60, Key_61, Key_62, Key_63, Key_64, Key_65, Key_66, Key_67, Key_68, Key_69, Key_70, Key_71,
+        Key_72, Key_73, Key_74, Key_75, Key_76, Key_77, Key_78, Key_79, Key_80, Key_81, Key_82, Key_83, 
+        Key_84, Key_85, Key_86, Key_87, Key_88, Key_89, Key_90, Key_91, Key_92, Key_93, Key_94, Key_95, 
+        Key_96, Key_97, Key_98, Key_99, Key_100, Key_101, Key_102, Key_103, Key_104, Key_105, Key_106, Key_107,
+        Key_108, Key_109, Key_110, Key_111, Key_112, Key_113, Key_114, Key_115, Key_116, Key_117, Key_118, Key_119,
+        Key_120, Key_121, Key_122, Key_123, Key_124, Key_125, Key_126, Key_127
+    }
+
+    public static class KeyExtensions
+    {
+        /// <summary>
+        /// Get key octave (0-11)
+        /// </summary>
+        /// <returns>Octave</returns>
+        public static int Octave(this Key key)
+        {
+            var keyValue = (int)key;
+            if (keyValue < 0 || keyValue > 127)
+                throw new ArgumentOutOfRangeException(nameof(key));
+
+            // 12 notes per octave
+            return keyValue / 12;
+        }
+
+        /// <summary>
+        /// Get key note (C-B)
+        /// </summary>
+        /// <returns>Note</returns>
+        public static Note Note(this Key key)
+        {
+            var keyValue = (int)key;
+            if (keyValue < 0 || keyValue > 127)
+                throw new ArgumentOutOfRangeException(nameof(key));
+
+            return (Note)(keyValue % 12);
+        }
+    }
+}
