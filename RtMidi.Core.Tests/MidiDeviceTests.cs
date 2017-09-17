92b20035debf36ede7b7dfdae0736f4c539439ae
Michael Dahl <micdah@gmail.com>
2017-09-17 18:59:39 +0200

    MidiDevice base

---
+++ RtMidi.Core.Tests/MidiDeviceTests.cs
@@ -0,0 +1,67 @@
+﻿using RtMidi.Core.Unmanaged.Devices;
+using Moq;
+using Xunit;
+
+namespace RtMidi.Core.Tests
+{
+    public class MidiDeviceTests
+    {
+        private readonly Mock<IRtMidiDevice> _rtMidiDevice;
+        private readonly MidiDevice<IRtMidiDevice> _sut;
+
+        public MidiDeviceTests()
+        {
+            _rtMidiDevice = new Mock<IRtMidiDevice>();
+            _sut = new MidiDevice<IRtMidiDevice>(_rtMidiDevice.Object);
+        }
+
+        [Fact]
+        public void Should_Delegate_IsOpen()
+        {
+            // Setup
+            _rtMidiDevice
+                .Setup(x => x.IsOpen)
+                .Returns(true)
+                .Verifiable("Should call IsOpen on RtMidiDevice");
+
+            // Test
+            var isOpen = _sut.IsOpen;
+
+            // Verify
+            Assert.True(isOpen);
+            _rtMidiDevice.Verify();
+        }
+
+        [Fact]
+        public void Should_Delegate_Open() 
+        {
+            // Setup
+            _rtMidiDevice
+                .Setup(x => x.Open())
+                .Returns(true)
+                .Verifiable("Should call Open on RtMidiDevice");
+
+            // Test
+            var wasOpened = _sut.Open();
+
+            // Verify
+            Assert.True(wasOpened);
+            _rtMidiDevice.Verify();
+        }
+
+        [Fact]
+        public void Should_Delegate_Close() 
+        {
+            // Setup
+            _rtMidiDevice
+                .Setup(x => x.Close())
+                .Verifiable("Should call Close on RtMidiDevice");
+
+            // Test
+            _sut.Close();
+
+            // Verify
+            _rtMidiDevice.Verify();
+        }
+    }
+}
---
+++ RtMidi.Core/MidiDevice.cs
@@ -0,0 +1,22 @@
+﻿using RtMidi.Core.Unmanaged.Devices;
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("RtMidi.Core.Tests")]
+
+namespace RtMidi.Core
+{
+    public class MidiDevice<TRtMidiDevice> where TRtMidiDevice : class, IRtMidiDevice
+    {
+        protected readonly TRtMidiDevice RtMidiDevice;
+
+        public MidiDevice(TRtMidiDevice rtMidiDevice)
+        {
+            RtMidiDevice = rtMidiDevice ?? throw new ArgumentNullException(nameof(rtMidiDevice));
+        }
+
+        public bool IsOpen => RtMidiDevice.IsOpen;
+        public bool Open() => RtMidiDevice.Open();
+        public void Close() => RtMidiDevice.Close();
+    }
+}
