b1c771da3ff17d8261e93b0fb529e63171fd0748
Michael Dahl <micdah@gmail.com>
2017-10-11 21:16:40 +0200

    Added appveyor.yml

---
+++ RtMidi.Core/IMidiDeviceInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace RtMidi.Core
+{
+    /// <summary>
+    /// Midi device info
+    /// </summary>
+    public interface IMidiDeviceInfo
+    {
+        /// <summary>
+        /// Name of midi device
+        /// </summary>
+        string Name { get; }
+    }
+}
---
+++ RtMidi.Core/MidiDeviceManager.cs
@@ -0,0 +1,45 @@
+﻿using RtMidi.Core.Unmanaged.Devices;
+using System;
+namespace RtMidi.Core
+{
+    public class MidiDeviceManager
+    {
+        public static readonly MidiDeviceManager Instance = new MidiDeviceManager();
+
+        public MidiDeviceManager()
+        {
+        }
+    }
+
+    internal class MidiDeviceInfo<TRtMidiDeviceInfo> : IMidiDeviceInfo
+        where TRtMidiDeviceInfo : RtMidiDeviceInfo
+    {
+        protected readonly TRtMidiDeviceInfo RtMidiDeviceInfo;
+
+        public MidiDeviceInfo(TRtMidiDeviceInfo rtMidiDeviceInfo) 
+        {
+            if (rtMidiDeviceInfo == null) throw new ArgumentNullException(nameof(rtMidiDeviceInfo));
+
+            RtMidiDeviceInfo = rtMidiDeviceInfo;
+        }
+
+        public string Name => RtMidiDeviceInfo.Name;
+    }
+
+    public interface IMidiInputDeviceInfo
+    {
+        IMidiInputDevice CreateDevice();
+    }
+
+    internal class MidiInputDeviceInfo : MidiDeviceInfo<RtMidiInputDeviceInfo>, IMidiInputDeviceInfo
+    {
+        public MidiInputDeviceInfo(RtMidiInputDeviceInfo rtMidiDeviceInfo) : base(rtMidiDeviceInfo)
+        {
+        }
+
+        public IMidiInputDevice CreateDevice()
+        {
+            return new MidiInputDevice(RtMidiDeviceInfo.CreateDevice());
+        }
+    }
+}
